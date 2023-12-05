356f7280f58729a87fe06872b92dea876d53d6ed
2joopiter <veryberrylike3@gmail.com>
2023-12-06 00:42:55 +0900

    gallery 기본 레이아웃 추가

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,10 +1,160 @@
+import { useEffect, useRef, useState } from 'react';
+import Masonry from 'react-masonry-component';
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
+import { LuSearch } from 'react-icons/lu';
+import Modal from '../../common/modal/Modal';
 
 export default function Gallery() {
+	const [Pics, setPics] = useState([]);
+	const myID = useRef('199646606@N06');
+	const refNav = useRef(null);
+	const isUser = useRef(myID.current);
+	const [Open, setOpen] = useState(false);
+	const [Index, setIndex] = useState(0);
+
+	const activateBtn = (e) => {
+		const btns = refNav.current.querySelectorAll('button');
+		btns.forEach((btn) => btn.classList.remove('on'));
+		e && e.target.classList.add('on');
+	};
+	const handleInterest = (e) => {
+		if (e.target.classList.contains('on')) return;
+		isUser.current = '';
+		activateBtn(e);
+		fetchFlickr({ type: 'interest' });
+	};
+	const handleMine = (e) => {
+		if (e.target.classList.contains('on') || isUser.current === myID.current) return;
+		isUser.current = myID.current;
+		activateBtn(e);
+		fetchFlickr({ type: 'user', id: myID.current });
+	};
+	const handleUser = (e) => {
+		if (isUser.current) return;
+		isUser.current = e.target.innerText;
+		activateBtn();
+		fetchFlickr({ type: 'user', id: e.target.innerText });
+	};
+
+	const handleSearch = (e) => {
+		e.preventDefault();
+		isUser.current = '';
+		activateBtn();
+		const keyword = e.target.children[0].value;
+		if (!keyword.trim()) return;
+		e.target.children[0].value = '';
+		fetchFlickr({ type: 'search', keyword: keyword });
+	};
+
+	const fetchFlickr = async (opt) => {
+		const num = 30;
+		const flickr_api = process.env.REACT_APP_FLICKR_API;
+		const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
+		const method_interest = 'flickr.interestingness.getList';
+		const method_user = 'flickr.people.getPhotos';
+		const method_search = 'flickr.photos.search';
+		const interestURL = `${baseURL}${method_interest}`;
+		const searchURL = `${baseURL}${method_search}&tags=${opt.keyword}`;
+		const userURL = `${baseURL}${method_user}&user_id=${opt.id}`;
+
+		let url = '';
+
+		opt.type === 'user' && (url = userURL);
+		opt.type === 'interest' && (url = interestURL);
+		opt.type === 'search' && (url = searchURL);
+
+		const data = await fetch(url);
+		const json = await data.json();
+
+		setPics(json.photos.photo);
+	};
+
+	useEffect(() => {
+		fetchFlickr({ type: 'user', id: myID.current });
+	}, []);
+
 	return (
-		<Layout title={'Gallery'}>
-			<p>Gallery 전용 컨텐츠</p>
-		</Layout>
+		<>
+			<Layout title={'Gallery'}>
+				<article className='controls'>
+					<nav className='btnSet' ref={refNav}>
+						<button onClick={handleInterest}>Interest Gallery</button>
+						<button className='on' onClick={handleMine}>
+							My Gallery
+						</button>
+					</nav>
+
+					<form onSubmit={handleSearch}>
+						<input type='text' placeholder='Search' />
+						<button className='btnSearch'>
+							<LuSearch />
+						</button>
+					</form>
+				</article>
+
+				<section>
+					<Masonry className={'frame'} options={{ transitionDuration: '0.5s', gutter: 5 }}>
+						{Pics.length === 0 ? (
+							<h2>해당 키워드에 해당하는 검색 결과가 없습니다.</h2>
+						) : (
+							Pics.map((pic, idx) => {
+								return (
+									<article key={pic.id}>
+										<div
+											className='pic'
+											onClick={() => {
+												setOpen(true);
+												setIndex(idx);
+											}}
+										>
+											<img
+												src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+												alt={pic.title}
+											/>
+										</div>
+
+										<section
+											className='infoBox'
+											onMouseover={(e) => {
+												e.target.style.backgroundColor = 'rgba(var(--baseColor-code), 0.5';
+											}}
+										>
+											<div className='profile'>
+												<img
+													src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+													alt='사용자 프로필 이미지'
+													onError={(e) =>
+														e.target.setAttribute(
+															'src',
+															'https://www.flickr.com/images/buddyicon.gif'
+														)
+													}
+												/>
+											</div>
+											<div className='text'>
+												<h2>{pic.title}</h2>
+												<span onClick={handleUser}>{pic.owner}</span>
+											</div>
+										</section>
+									</article>
+								);
+							})
+						)}
+					</Masonry>
+				</section>
+			</Layout>
+
+			{
+				<Modal Open={Open} setOpen={setOpen}>
+					{Pics.length !== 0 && (
+						<img
+							src={`https://live.staticflickr.com/${Pics[Index].server}/${Pics[Index].id}_${Pics[Index].secret}_b.jpg`}
+							alt={Pics[Index].title}
+						/>
+					)}
+				</Modal>
+			}
+		</>
 	);
 }
