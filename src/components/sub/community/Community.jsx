b1c53f33be123eaa724f54cb3ff2acfab8f02e5c
2joopiter <veryberrylike3@gmail.com>
2023-12-13 23:41:39 +0900

    community, contact 기본 배치

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -1,10 +1,158 @@
+import { useEffect, useRef, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Community.scss';
+import { MdClose } from 'react-icons/md';
+import { IoMdCreate } from 'react-icons/io';
+import { useCustomText } from '../../../hooks/useText';
 
 export default function Community() {
+	const changeText = useCustomText('combined');
+	const getLocalData = () => {
+		const data = localStorage.getItem('post');
+		return JSON.parse(data);
+	};
+	const [Post, setPost] = useState(getLocalData());
+	const refTit = useRef(null);
+	const refCon = useRef(null);
+	const refEditTit = useRef(null);
+	const refEditCon = useRef(null);
+	const editMode = useRef(false);
+
+	//input 초기화 함수
+	const resetPost = () => {
+		refTit.current.value = '';
+		refCon.current.value = '';
+	};
+
+	//글 저장 함수
+	const createPost = () => {
+		if (!refTit.current.value.trim() || !refCon.current.value.trim()) {
+			resetPost();
+			return alert('제목과 본문을 모두 입력하세요.');
+		}
+		const korTime = new Date().getTime() + 1000 * 60 * 60 * 9;
+		setPost([
+			{ title: refTit.current.value, content: refCon.current.value, date: new Date(korTime) },
+			...Post,
+		]);
+		resetPost();
+	};
+
+	//글 수정 함수
+	const updatePost = (updateIndex) => {
+		if (!refEditTit.current.value.trim() || !refEditCon.current.value.trim()) {
+			return alert('수정할 글의 제목과 본문을 모두 입력하세요.');
+		}
+		editMode.current = false;
+
+		setPost(
+			Post.map((el, idx) => {
+				if (updateIndex === idx) {
+					el.title = refEditTit.current.value;
+					el.content = refEditCon.current.value;
+					el.enableUpdate = false;
+				}
+				return el;
+			})
+		);
+	};
+
+	//글 삭제 함수
+	const deletePost = (delIndex) => {
+		if (!window.confirm('정말 해당 게시글을 삭제하겠습니까?')) return;
+		setPost(Post.filter((_, idx) => delIndex !== idx));
+	};
+
+	//수정모드 변경함수
+	const enableUpdate = (editIndex) => {
+		if (editMode.current) return;
+		editMode.current = true;
+		setPost(
+			Post.map((el, idx) => {
+				if (editIndex === idx) el.enableUpdate = true;
+				return el;
+			})
+		);
+	};
+
+	//출력모드 변경함수
+	const disableUpdate = (editIndex) => {
+		editMode.current = false;
+		setPost(
+			Post.map((el, idx) => {
+				if (editIndex === idx) el.enableUpdate = false;
+				return el;
+			})
+		);
+	};
+
+	useEffect(() => {
+		Post.map((el) => (el.enableUpdate = false));
+		localStorage.setItem('post', JSON.stringify(Post));
+	}, [Post]);
+
 	return (
 		<Layout title={'Community'}>
-			<p>Community 전용 컨텐츠</p>
+			<div className='communityWrap'>
+				<div className='inputBox'>
+					<input type='text' placeholder='title' ref={refTit} />
+					<textarea cols='30' rows='3' placeholder='content' ref={refCon}></textarea>
+
+					<nav>
+						<button onClick={createPost}>
+							<IoMdCreate />
+						</button>
+						<button onClick={resetPost}>
+							<MdClose />
+						</button>
+					</nav>
+				</div>
+
+				<div className='showBox'>
+					{Post.map((el, idx) => {
+						const date = JSON.stringify(el.date);
+						const strDate = changeText(date.split('T')[0].slice(1), '.');
+
+						if (el.enableUpdate) {
+							//수정모드
+							return (
+								<article key={el + idx}>
+									<div className='txt'>
+										<input type='text' defaultValue={el.title} ref={refEditTit} />
+										<textarea
+											cols='30'
+											rows='4'
+											defaultValue={el.content}
+											ref={refEditCon}
+										></textarea>
+										<span>{strDate}</span>
+									</div>
+									<nav>
+										{/* 수정모드 일때 해당 버튼 클릭시 다시 출력모드 변경 */}
+										<button onClick={() => disableUpdate(idx)}>Cancel</button>
+										<button onClick={() => updatePost(idx)}>Update</button>
+									</nav>
+								</article>
+							);
+						} else {
+							//출력모드
+							return (
+								<article key={el + idx}>
+									<div className='txt'>
+										<h2>{el.title}</h2>
+										<p>{el.content}</p>
+										<span>{strDate}</span>
+									</div>
+									<nav>
+										<button onClick={() => enableUpdate(idx)}>Edit</button>
+										<button onClick={() => deletePost(idx)}>Delete</button>
+									</nav>
+								</article>
+							);
+						}
+					})}
+				</div>
+			</div>
 		</Layout>
 	);
 }
--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,10 +1,180 @@
+import { useEffect, useRef, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+	const form = useRef();
+	const resetForm = () => {
+		const elArr = form.current.children;
+
+		Array.from(elArr).forEach((el) => {
+			console.log(el);
+			if (el.name === 'user_name' || el.name === 'user_email' || el.name === 'message')
+				el.value = '';
+		});
+	};
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		const [user, email] = form.current.querySelectorAll('input');
+		const txtArea = form.current.querySelector('textarea');
+
+		if (!user.value || !email.value || !txtArea.value)
+			return alert('이름, 답장받을 이메일주소 문의내용을 모두 입력하세요.');
+
+		emailjs.sendForm('service_zzree4j', 'template_w86wuw7', form.current, '5euWzAafCXgbAmv3z').then(
+			(result) => {
+				alert('문의 내용이 성공적으로 전송되었습니다.');
+				resetForm();
+			},
+			(error) => {
+				alert('일시적인 장애로 문의 전송에 실패했습니다. 다음의 메일주소로 보내주세요.');
+				resetForm();
+			}
+		);
+	};
+
+	const kakao = useRef(window.kakao);
+
+	const [Index, setIndex] = useState(0);
+	const [Traffic, setTraffic] = useState(false);
+	const [View, setView] = useState(false);
+
+	const mapFrame = useRef(null);
+	const viewFrame = useRef(null);
+
+	const marker = useRef(null);
+	const mapInstance = useRef(null);
+
+	const mapInfo = useRef([
+		{
+			title: '여의도 IFC몰',
+			latlng: new kakao.current.maps.LatLng(37.52506188634506, 126.9259552665427),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
+		},
+		{
+			title: '삼성역 코엑스',
+			latlng: new kakao.current.maps.LatLng(37.51100661425726, 127.06162026853143),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
+		},
+		{
+			title: '서울 시청',
+			latlng: new kakao.current.maps.LatLng(37.5662952, 126.9779451),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
+		},
+	]);
+
+	marker.current = new kakao.current.maps.Marker({
+		position: mapInfo.current[Index].latlng,
+		image: new kakao.current.maps.MarkerImage(
+			mapInfo.current[Index].imgSrc,
+			mapInfo.current[Index].imgSize,
+			mapInfo.current[Index].imgOpt
+		),
+	});
+
+	const roadview = () => {
+		new kakao.current.maps.RoadviewClient().getNearestPanoId(
+			mapInfo.current[Index].latlng,
+			50,
+			(panoId) => {
+				new kakao.current.maps.Roadview(viewFrame.current).setPanoId(
+					panoId,
+					mapInfo.current[Index].latlng
+				);
+			}
+		);
+	};
+
+	const setCenter = () => {
+		mapInstance.current.setCenter(mapInfo.current[Index].latlng);
+		roadview();
+	};
+
+	useEffect(() => {
+		mapFrame.current.innerHTML = '';
+		mapInstance.current = new kakao.current.maps.Map(mapFrame.current, {
+			center: mapInfo.current[Index].latlng,
+			level: 3,
+		});
+		marker.current.setMap(mapInstance.current);
+		setTraffic(false);
+		setView(false);
+
+		roadview();
+		mapInstance.current.addControl(
+			new kakao.current.maps.MapTypeControl(),
+			kakao.current.maps.ControlPosition.TOPRIGHT
+		);
+
+		mapInstance.current.addControl(
+			new kakao.current.maps.ZoomControl(),
+			kakao.current.maps.ControlPosition.RIGHT
+		);
+
+		mapInstance.current.setZoomable(false);
+
+		window.addEventListener('resize', setCenter);
+		return () => window.removeEventListener('resize', setCenter);
+	}, [Index]);
+
+	useEffect(() => {
+		Traffic
+			? mapInstance.current.addOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC)
+			: mapInstance.current.removeOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC);
+	}, [Traffic]);
+
 	return (
 		<Layout title={'Contact'}>
-			<p>Contact 전용 컨텐츠</p>
+			<h2>Message</h2>
+			<article className='msgBox'>
+				<div id='mailSection'>
+					<form ref={form} onSubmit={sendEmail}>
+						<label>Name</label>
+						<input type='text' name='user_name' />
+						<label>Email</label>
+						<input type='email' name='user_email' />
+						<label>Message</label>
+						<textarea name='message' />
+						<div className='sendBtn'>
+							<input type='submit' value='Send' />
+						</div>
+					</form>
+				</div>
+			</article>
+
+			<h2>MAP</h2>
+
+			<div id='mapSection'>
+				<div className='controlBox'>
+					<nav className='branch'>
+						{mapInfo.current.map((el, idx) =>
+							//prettier-ignore
+							<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>{el.title}</button>
+						)}
+					</nav>
+
+					<nav className='info'>
+						<button onClick={() => setTraffic(!Traffic)}>
+							{Traffic ? '교통정보 OFF' : '교통정보 ON'}
+						</button>
+						<button onClick={() => setView(!View)}>{View ? '지도' : '로드뷰'}</button>
+						<button onClick={setCenter}>위치 초기화</button>
+					</nav>
+				</div>
+				<section className='tab'>
+					<article className={`mapBox ${View ? '' : 'on'}`} ref={mapFrame}></article>
+					<article className={`viewBox ${View ? 'on' : ''}`} ref={viewFrame}></article>
+				</section>
+			</div>
 		</Layout>
 	);
 }
