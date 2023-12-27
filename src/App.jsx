cfa3e68977a5563f9f57d1a577b8a4633769ce77
2joopiter <veryberrylike3@gmail.com>
2023-12-27 17:08:41 +0900

    초기세팅

--- src/App.jsx
+++ src/App.jsx
@@ -14,25 +14,31 @@ import './globalStyles/Variables.scss';
 import './globalStyles/Reset.scss';
 import { useState } from 'react';
 import { useMedia } from './hooks/useMedia';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Menu from './components/common/menu/Menu';
 
 export default function App() {
 	const [Dark, setDark] = useState();
 	const [Toggle, setToggle] = useState(false);
+	const queryClient = new QueryClient();
 
 	return (
-		<div className={`wrap ${Dark ? 'dark' : ''} ${useMedia()}`}>
-			<Header Dark={Dark} setDark={setDark} Toggle={Toggle} setToggle={setToggle} />
-			<Route exact path='/' component={MainWrap} />
-			<Route path='/Community' component={Community} />
-			<Route path='/Contact' component={Contact} />
-			<Route path='/Department' component={Department} />
-			<Route path='/Gallery' component={Gallery} />
-			<Route path='/Members' component={Members} />
-			<Route path='/Youtube' component={Youtube} />
-			<Route path='/Detail/:id' component={Detail} />
-			<Footer />
-			{Toggle && <Menu setToggle={setToggle} />}
-		</div>
+		<QueryClientProvider client={queryClient}>
+			<div className={`wrap ${Dark ? 'dark' : ''} ${useMedia()}`}>
+				<Header Dark={Dark} setDark={setDark} Toggle={Toggle} setToggle={setToggle} />
+				<Route exact path='/' component={MainWrap} />
+				<Route path='/Community' component={Community} />
+				<Route path='/Contact' component={Contact} />
+				<Route path='/Department' component={Department} />
+				<Route path='/Gallery' component={Gallery} />
+				<Route path='/Members' component={Members} />
+				<Route path='/Youtube' component={Youtube} />
+				<Route path='/Detail/:id' component={Detail} />
+				<Footer />
+				{Toggle && <Menu setToggle={setToggle} />}
+			</div>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	);
 }
